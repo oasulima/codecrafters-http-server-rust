c527f581b3a4ce109bc7ce86ef545c9e2b86ec90
Oleg Sulima <oleg.sulima@outlook.com>
2024-03-20 22:48:39 +0300

    Post a file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use regex::Regex;
 use std::{
     fs,
-    io::{BufRead, BufReader, Write},
+    io::{BufRead, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
     thread,
 };
@@ -39,11 +39,9 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_lines = buf_reader.lines();
+    // let request_lines = buf_reader.lines();
 
-    let (status_line, content_type, content) = parse_request_line(request_lines);
-
-    println!("content: {content}");
+    let (status_line, content_type, content) = parse_request_line(buf_reader);
 
     let length = content.len();
 
@@ -57,52 +55,90 @@ fn handle_connection(mut stream: TcpStream) {
 }
 
 fn parse_request_line(
-    mut request_lines: std::io::Lines<BufReader<&mut TcpStream>>,
+    mut buf_reader: BufReader<&mut TcpStream>,
 ) -> (&'static str, &'static str, String) {
-    let request_line = request_lines
-        .next()
-        .expect("error: Empty request")
-        .expect("error: couldn't parse request line");
+    let mut request_line = String::new();
+    buf_reader
+        .read_line(&mut request_line)
+        .expect("error: couldn't read request line");
 
-    let re = Regex::new(r"GET /(?<path>\S*) HTTP/1.1").expect("error: Couldn't compile regex");
+    let re =
+        Regex::new(r"(?<method>\S+) (?<path>\S+) HTTP/1.1").expect("error: Couldn't compile regex");
 
     if re.is_match(&request_line) {
-        let (_, [path]) = re
+        let (_, [method, path]) = re
             .captures(&request_line)
             .expect("error: Couldn't get path")
             .extract();
 
-        println!("path: {path}");
-
-        if path.is_empty() {
+        if path == "/" {
             return ("HTTP/1.1 200 OK", "text/plain", "".to_string());
         }
-        if path == "user-agent" {
-            while let Some(request_line) = request_lines.next() {
-                let request_line = request_line.expect("error: couldn't parse request line");
+        if path == "/user-agent" {
+            loop {
+                let mut request_line = String::new();
+                let r = buf_reader
+                    .read_line(&mut request_line)
+                    .expect("error: couldn't read request line");
+                if r < 3 {
+                    //detect empty line
+                    break;
+                }
                 if request_line.starts_with("User-Agent:") {
                     return (
                         "HTTP/1.1 200 OK",
                         "text/plain",
-                        (&request_line[12..]).to_string(),
+                        (&request_line[12..]).trim().to_string(),
                     );
                 }
             }
         }
-        if path.starts_with("echo/") {
-            return ("HTTP/1.1 200 OK", "text/plain", (&path[5..]).to_string());
+        if path.starts_with("/echo/") {
+            return ("HTTP/1.1 200 OK", "text/plain", (&path[6..]).to_string());
         }
-        if path.starts_with("files/") {
+        if path.starts_with("/files/") {
             let args = Args::parse();
 
             let dir = args.directory;
-            let file_name = &path[6..];
+            let file_name = &path[7..];
 
             let file_path = format!("{dir}/{file_name}");
 
-            let file = fs::read_to_string(file_path);
-            if file.is_ok() {
-                return ("HTTP/1.1 200 OK", "application/octet-stream", file.unwrap());
+            match method {
+                "GET" => {
+                    let file = fs::read_to_string(file_path);
+                    if file.is_ok() {
+                        return ("HTTP/1.1 200 OK", "application/octet-stream", file.unwrap());
+                    }
+                }
+                "POST" => {
+                    let mut content_length = 0;
+                    loop {
+                        let mut request_line = String::new();
+                        let r = buf_reader
+                            .read_line(&mut request_line)
+                            .expect("error: couldn't read request line");
+                        if r < 3 {
+                            //detect empty line
+                            break;
+                        }
+                        if request_line.starts_with("Content-Length:") {
+                            content_length = (&request_line[15..])
+                                .trim()
+                                .parse::<usize>()
+                                .expect("error: couldn't parse content-length");
+                        }
+                    }
+
+                    let mut file_content = vec![0; content_length]; //New Vector with size of Content
+                    buf_reader
+                        .read_exact(&mut file_content)
+                        .expect("error: couldn't read body");
+
+                    fs::write(file_path, file_content).expect("error: couldn't write the file");
+                    return ("HTTP/1.1 201 OK", "text/plain", "".to_string());
+                }
+                &_ => {}
             }
         }
     }
